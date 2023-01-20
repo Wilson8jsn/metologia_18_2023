62954c156b9702c6497cdd0497fdd34e05668d63
Wilson8jsn <dc105677543@gmail.com>
2023-01-20 06:26:56 -0500

    frist commit

---
+++ calificaciones.js
@@ -0,0 +1,13 @@
+let nums = [23,45,75]
+for(i = 0; i>nums.length; i++){
+
+    console.log(nums[i]);
+
+if(nums>70||nums>70)
+    {
+    console.log("reprobo")
+    }
+else{
+    console.log("aprobo")
+}
+}
